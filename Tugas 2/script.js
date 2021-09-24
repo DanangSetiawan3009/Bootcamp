1958ed8120047aea697bfde66cb39924bb52acc0
DanangSetiawan3009 <danangsetiawan648@gmail.com>
2021-09-24 18:14:35 +0700

    Tugas 2

---
+++ Tugas 2/script.js
@@ -0,0 +1,8 @@
+function klikTombol() {
+    const username = document.querySelector("#username")
+    const password = document.querySelector("#password")
+    console.log(username.value);
+    console.log(password.value);
+    // let confirm = document.querySelector("#confirm")
+    // confirm.innerHTML = '<a href="http://127.0.0.1:5500/Tugas%201/index.html">Confirm</a>'
+}
